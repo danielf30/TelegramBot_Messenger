04d74f98f2554916ece047083625260bab761153
Daniel Fajardo <danielf.fajardop30@gmail.com>
2023-01-30 23:28:30 -0500

    Actualización de valores secretos

--- send-messages-timertrigger/__init__.py
+++ send-messages-timertrigger/__init__.py
@@ -11,15 +11,10 @@ import pytz
 num_messages_morning = 1
 num_messages_night = 1
 
-#secret_value = os.environ.get('token_bot')
-#bot_token = telegram.Bot(token=secret_value)
-#secrets = {'chatbot_id1': os.environ.get('chatbot_id1'), 'chatbot_id2': os.environ.get('chatbot_id2')}
-#secret_value1, secret_value2 = secrets.values()
-#chat_ids = [secret_value1,secret_value2]
-
-bot_token = telegram.Bot(token='5743813753:AAFuPL5KTJeOZLC1UpL5CCn2Ggv-hZbZSi4')
-#updates = await bot_token.get_updates()
-chat_ids = [919917985]#747861357
+secret_value = os.environ.get('token_bot')
+bot_token = telegram.Bot(token=secret_value)
+chat_id2 = os.environ.get('chatbot_id2')
+chat_ids = [chat_id2]
 
 async def send_morning_messages(chat_ids, bot):
     url = 'https://www.wishesmsg.com/good-morning-messages-for-girlfriend/'
